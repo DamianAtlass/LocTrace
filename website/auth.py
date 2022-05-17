a99da87fc927662c28ac11dad1a738247672c773
DamianAtlass <damianatlass@gmx.de>
2022-05-17 10:44:34 +0200

    checks if user exists and if it is unique

--- website/auth.py
+++ website/auth.py
@@ -10,20 +10,34 @@ df = pd.read_csv("logindata.csv")
 @auth.route("/login/", methods=["GET", "POST"])
 def login():
     if request.method =="POST":
+
         username = request.form.get("username")
-        pw = request.form.get("password")
-        print("username: "+ username+" pw: "+pw)
-        # get right row
+        password_login = request.form.get("password")
+        #print("username: "+ username+" pw: "+pw)
+
+        # get user-password pair for corresponding username
         data_pair = df.loc[df['username'] == username ]
+
+        #check if user exists and if it is unique
+        len = data_pair.shape[0]  # Gives number of rows
+        
+        if len < 1:
+            return "ERROR: user doesn't exist"
+        
+        if len > 1:
+            return "ERROR: multiple users with same username found"
+
+        
 
         # get right column
         password_df = data_pair.iloc[0, 1]
+
         
-        if(pw == password_df):
+        
+        if(password_login == password_df):
             return render_template("map.html", username = username)
-            #("map.html", foo = variable)
         else:
-             return "not logged in"
+             return "password incorrect"
 
 
     if request.method =="GET":
