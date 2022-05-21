c361c4c8ba79c0cbe25acffb3bf325c17b5ec329
DamianAtlass <damianatlass@gmx.de>
2022-05-21 13:03:52 +0200

    removed user.py because it was not needed

--- website/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-  def __init__(self, username):
-    self.username = username
-    self.is_authenticated = None
-    self.is_active = None
-  def get_id(self):
-    return self.username
