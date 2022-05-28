66013090a6a8261f6003250c7bcd92aef37f7f11
Your Name <jiwoop2019uni@gmail.com>
2022-05-28 11:28:05 +0200

    new design branch with css

--- website/views.py
+++ website/views.py
@@ -13,14 +13,14 @@ def base():
 
 variable = "variables can be passed this way"
 
-@views.route("/map")
+@views.route("/map/")
 def map():
     temp = render_template("map.html", foo = variable)
     return temp
 
 foo = "hey"
 
-@views.route("/login")
+@views.route("/login/")
 def login():
     temp = render_template("login.html")
     return temp
