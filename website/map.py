9557aceabb4fcee746ac25cdfcb30d9fdd05a89f
Tessa Heidkamp <tessaheidkamp@MacBook-Pro-von-Tessa.local>
2022-05-30 12:04:58 +0200

    build_date_ maps filter

--- website/map.py
+++ website/map.py
@@ -19,12 +19,15 @@ from datetime import datetime
 # function for building the map with given data
 def buildmap(user):
 
+    '''
     userStr = str(user)[1:-1].replace(" ","")
     #print(str(user))
     print(type(userStr)) 
     print(userStr)
-
-    csv_path = "website/data/" + userStr + ".csv"
+    print("username: " +  user.username)
+    '''
+   
+    csv_path = "website/data/" + user.username + ".csv"
     
     data = pd.read_csv(csv_path)
 
@@ -61,13 +64,15 @@ def build_weekday_map(user, weekday):
     # mapping int to weekday
     weekDays = {0: "Monday", 1 : "Tuesday", 2 : "Wednesday", 3:  "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
     
+    '''
     # get Stringformatted Username
     userStr = str(user)[1:-1].replace(" ","")
     #for debugging delete later <---
     print(type(userStr)) 
     print(userStr)
-    
-    csv_path = "website/data/" + userStr + ".csv"
+     '''
+  
+    csv_path = "website/data/" + user.username + ".csv"
     
     # get Data for user 
     data = pd.read_csv(csv_path)
@@ -112,6 +117,58 @@ def build_weekday_map(user, weekday):
     print("mapPath:" +mapPath)
     m3.save(mapPath)
 
+# expl week, needs to be adjusted {"2021-10-25" : 0 , "2021-10-26" : 1, "2021-10-27" : 2, "2021-10-28" : 3, "2021-10-29" : 4, "2021-10-30" : 5,"2021-10-31" : 6}
+def build_date_map(user, req_start_date, req_end_date): 
+    
+    # creating comaprable ints form string dates
+    date_values = {"2021-10-25" : 0 , "2021-10-26" : 1, "2021-10-27" : 2, "2021-10-28" : 3, "2021-10-29" : 4, "2021-10-30" : 5,"2021-10-31" : 6}
+    start_date_value = date_values[req_start_date]
+    end_date_value = date_values[req_end_date]
+  
+    csv_path = "website/data/" + user.username + ".csv"
+    
+    # get Data for user 
+    data = pd.read_csv(csv_path)
+    newData = pd.DataFrame()
+
+    for i in range(0, len(data)-1):
+        dataDate = data['ts'].iloc[i]
+        #vllt problem, wegen :00
+        # date from timestamp in data
+        data_date = datetime.strptime(dataDate, '%Y-%m-%d %H:%M:%S+%f:00').date()
+       
+        # if required date, add to newDataframe
+        if start_date_value <= date_values[str(data_date)] and date_values[str(data_date)] <= end_date_value:
+            newData = pd.concat([newData, data.iloc[[i]]])
+
+    # create new folium map with filterd Data
+    location = newData['latitude'].mean(), newData['longitude'].mean()
+    print(location)
+
+    m3 = folium.Map(
+        location,
+        zoom_start=15)
+
+    colormap = cm.LinearColormap(colors=['darkblue', 'blue', 'green', 'yellow', 'orange', 'red'],
+                             index=[0, 100, 250, 500, 700, 1000], vmin=0, vmax=1000,
+                             caption='motion score')
+    m3.add_child(colormap)
+
+    
+    for i in range(0, len(newData)-1):
+        loc1 = newData['latitude'].iloc[i],newData['longitude'].iloc[i]
+        loc2 = newData['latitude'].iloc[i+1],newData['longitude'].iloc[i+1]
+
+        loc = [loc1,loc2]
+    
+        color_ = colormap(newData['motion_score'][i])
+
+        folium.PolyLine(loc, weight=5, opacity=1, color=color_).add_to(m3)
+
+
+    mapPath = "website/templates/map_date.html" 
+    print("mapPath:" +mapPath)
+    m3.save(mapPath)
    
 
 
--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import  login_required, current_user
 from website.map import buildmap
 from website.map import build_weekday_map
+from website.map import build_date_map
 from datetime import datetime
 import folium
 import sys
@@ -22,13 +23,21 @@ variable = "variables can be passed this way"
 
 @views.route("/map/", methods=['GET','POST'])
 def map():
+
+    if request.method == 'POST' : 
+        print("Filter button POST")
         
+        #getting values from html form 
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+        build_date_map(current_user, start_date, end_date)
+        temp = render_template("map_date.html")
         #print(str(start), file=sys.stdout)
+        return temp
+    
     buildmap(current_user)
     temp = render_template("map.html")
     return temp
-    
-  
   
     
 
@@ -43,8 +52,11 @@ def map1():
 def base():
     return render_template("base.html", user = current_user)
 
-# example for routing for filteroption "monday" <- 
+# example for routing for filteroption "wednesday" <- 
 @views.route("/wednesday/")
 def weekday():
     build_weekday_map(current_user, "Wednesday")
     return render_template("mapWednesday.html")
+
+
+
