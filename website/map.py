0b3fe30626ccaa4ae38ea19341986452b6e2ec2b
DamianAtlass <damianatlass@gmx.de>
2022-06-09 20:05:00 +0200

    work and home location will now be displayed on map

--- website/map.py
+++ website/map.py
@@ -21,66 +21,164 @@ from geopy.geocoders import Nominatim
 #loading all the loc data
 #using panda dataframe
 
+#functions
+
 # converts a str to a date
 def toDate(str):
     return datetime.strptime(str[0:-6], "%Y-%m-%d %H:%M:%S")
 
 
-#returns location of work
+#returns location of home
 def getHomeLoc(stops):
     
     durationPerUniqueId = np.zeros(len(stops)-1, dtype=np.int64)
     
-    for i in range(0, len(stops)-1):
-        durationPerUniqueId[stops.iloc[i]["unique_id"]] += stops.iloc[i]["duration"]
-            
-    max_index = np.argmax(durationPerUniqueId)
-    #print("max: "+str(max_index))
-    home = None
-    
-    for i in range(0, len(stops)-1):
-        #print("id: "+str(stops.iloc[i]["unique_id"]))
-        if stops.iloc[i]["unique_id"] == max_index:
-            home = stops.iloc[i]
-            break
-    return home
-
-#returns location of work
-def getWorkLoc(stops, home):
-    
-    durationPerUniqueId = np.zeros(len(stops)-1, dtype=np.int64)
-    
-    for i in range(0, len(stops)-1):
-        
-        if stops.iloc[i]["unique_id"] != home["unique_id"] and toDate(stops.iloc[i]["start"]).weekday() < 5:
-            #print("weekday: "+str(toDate(stops.iloc[i]["start"]).weekday()))
+    for i in range(0, len(stops)):
             durationPerUniqueId[stops.iloc[i]["unique_id"]] += stops.iloc[i]["duration"]
             
     max_index = np.argmax(durationPerUniqueId)
+    #print("max: "+str(max_index))
+    longest_visited_place = None
     
-    workplace = None
-    
-    for i in range(0, len(stops)-1):
-        #print("id: "+str(stops.iloc[i]["unique_id"]))
+    for i in range(0, len(stops)):
         if stops.iloc[i]["unique_id"] == max_index:
-            workplace = stops.iloc[i]
+            longest_visited_place = stops.iloc[i]
             break
+    return longest_visited_place
+
+def diff_id(loc1, loc2):
+    if loc2 is None:
+        return True
+    if loc1["unique_id"] != loc2["unique_id"]:
+        return True
+    return False
+
+def getDate(data):
+    return toDate(data["start"])
+
+# gets a pandas dataframe
+# gives longest visited place
+
+
+#returns an array of possible work locations
+def getWorkLoc(stops, home):
+        
+    durationPerUniqueId = np.zeros(len(stops)-1, dtype=np.int64)
+    
+    #add all durations sorted after unique_id (if it is not home's id)
+    for i in range(0, len(stops)):
+        
+        if diff_id(stops.iloc[i], home) and toDate(stops["start"].iloc[i]).weekday() < 5:
+            #print("weekday: "+str(toDate(stops.iloc[i]["start"]).weekday()))
+            durationPerUniqueId[stops.iloc[i]["unique_id"]] += stops.iloc[i]["duration"]
+    #get index (= unique_id) of biggest entry
+    max_index = np.argmax(durationPerUniqueId)
+    
+    workplace = []
+    
+    #find first entry calculated unique_id and take it as work location
+    for i in range(0, len(stops)):
+        if stops.iloc[i]["unique_id"] == max_index:
+            workplace.append(stops.iloc[i])
+            break
+    #get total time at work in hours
+    time_worked = durationPerUniqueId[max_index]/(60*60)
+    
+    number_workdays = 0
+    
+    # calculate the number of work days
+    # (not a nice way to do it this way, but since we're only working with relativly small data it should be fine)
+    curr1 = getDate(stops.iloc[0])
+    
+    for i in range(0, len(stops)):
+        #if stops.iloc[i] has different date than curr1
+        if (getDate(stops.iloc[i]).date() - curr1.date()) != timedelta(days=0):
+            
+            #if this is a workday
+            if getDate(stops.iloc[i]).weekday()<5:
+                number_workdays+=1
+
+            #set curr to the next day
+            curr1 = curr1 + timedelta(days=1)
+        
+    #to prevent errors
+    if number_workdays == 0:
+        number_workdays = 1
+
+    # get average worktime per (work)day in hours
+    avr_work = time_worked/number_workdays
+    
+    # (2*8) / 5 = 3.2 ->two days fulltime work at the same place will not trigger this yet
+    if avr_work < 3:
+        
+        workplace = []
+        curr2 = getDate(stops.iloc[0])
+        temp = []
+        max_index = None
+        
+        #go over all stops
+        for i in range(0, len(stops)):
+            #if stops.iloc[i] has different date than curr2
+            if (getDate(stops.iloc[i]).date() - curr2.date()) != timedelta(days=0):
+                
+                #if this is a workday
+                if len(temp)>0 and getDate(temp[0]).weekday()<5:
+                    durationPerUniqueId = np.zeros(len(stops)-1, dtype=np.int64)
+                    
+                    #go over entries in temp
+                    for i in range(0, len(temp)):
+                        #if it's not the calculated home
+                        if diff_id(temp[i], home):
+                            #add on index ["unique_id"] to durationPerUniqueId[]
+                            durationPerUniqueId[temp[i]["unique_id"]] += temp[i]["duration"]
+                            
+                    #get index (->"unique_id") of biggest number
+                    max_index = np.argmax(durationPerUniqueId)
+                    
+                    
+                    
+                    #get first element in temp with said unique_id
+                    for i in range(0, len(temp)):
+                        
+                        # np.argmax can STILL return home["unique_id"], if the biggest values in durationPerUniqueId are
+                        # the same - if all values are 0, than 0 (as the first occurence) will be returned
+                        # this means we still need to filter home["unique_id"] out!
+                        if max_index != home["unique_id"] and temp[i]["unique_id"] == max_index:
+                            
+                            #add to other calculated places of work, reset max_index
+                            workplace.append(temp[i])
+                            max_index = None
+                            break
+                    
+                #set curr2 to the next day and reset temp
+                curr2 = curr2 + timedelta(days=1)
+                temp = []
+                temp.append(stops.iloc[i])
+                
+            #if same day
+            else:
+                temp.append(stops.iloc[i])
+                
+
     return workplace
 
+
+
 #builds a little popup
-def buildPopup(lat, long):
+def buildPopup(entry, showLastVisit):
     geolocator = Nominatim(user_agent="LocTrace")
-    adress, coordinates = geolocator.reverse(str(lat)+" "+str(long))
+    adress, coordinates = geolocator.reverse(str(entry["latitude"])+" "+str(entry["longitude"]))
     if adress == None:
         return None
+    html = ""
+    if showLastVisit:
+        html += "Besucht am: " + str(getDate(entry))+"<br><br>"
     
-    html = str(adress)
-    iframe = folium.IFrame(html,width=200,  height=100)
+    html += "Adresse:<br>"+str(adress)
+    iframe = folium.IFrame(html,width=200,  height=200)
 
     popup = folium.Popup(iframe, max_width=200)
     return popup
-
-
 
 
 # function for building the map with given data
@@ -122,16 +220,23 @@ def buildmap(user):
 
         folium.PolyLine(loc, weight=5, opacity=1, color=color_).add_to(m3)
 
+
+    #calculate home and work location
     home = getHomeLoc(stops)
     workplace = getWorkLoc(stops, home)
 
     # marker home
-    popup_h = buildPopup(home["latitude"], home["longitude"])
+    popup_h = buildPopup(home, False)
     folium.Marker((home["latitude"], home["longitude"]), icon=folium.Icon(icon='home',color='blue'), popup = popup_h).add_to(m3)
 
-    # marker work
-    popup_w = buildPopup(workplace["latitude"], workplace["longitude"])
-    folium.Marker((workplace["latitude"], workplace["longitude"]), icon=folium.Icon(icon='wrench',color='red'), popup = popup_w).add_to(m3)
+
+    for entry in workplace:#print("lat: "+str(entry["latitude"])+" | long: "+str(entry["longitude"])+" id:"+str(entry["unique_id"]))
+        if len(workplace)>1:
+            popup_w = buildPopup(entry, True)
+        else:
+            popup_w = buildPopup(entry, False)
+        folium.Marker((entry["latitude"], entry["longitude"]), icon=folium.Icon(icon='wrench',color='red'), popup = popup_w).add_to(m3)
+
 
 
     m3.save('website/templates/map1.html')
@@ -195,16 +300,22 @@ def build_weekday_map(user, weekday):
 
         folium.PolyLine(loc, weight=5, opacity=1, color=color_).add_to(m3)
 
+    #calculate home and work location
     home = getHomeLoc(stops)
     workplace = getWorkLoc(stops, home)
 
     # marker home
-    popup_h = buildPopup(home["latitude"], home["longitude"])
+    popup_h = buildPopup(home, False)
     folium.Marker((home["latitude"], home["longitude"]), icon=folium.Icon(icon='home',color='blue'), popup = popup_h).add_to(m3)
 
-    # marker work
-    popup_w = buildPopup(workplace["latitude"], workplace["longitude"])
-    folium.Marker((workplace["latitude"], workplace["longitude"]), icon=folium.Icon(icon='wrench',color='red'), popup = popup_w).add_to(m3)
+
+    for entry in workplace:#print("lat: "+str(entry["latitude"])+" | long: "+str(entry["longitude"])+" id:"+str(entry["unique_id"]))
+        if len(workplace)>1:
+            popup_w = buildPopup(entry, True)
+        else:
+            popup_w = buildPopup(entry, False)
+        folium.Marker((entry["latitude"], entry["longitude"]), icon=folium.Icon(icon='wrench',color='red'), popup = popup_w).add_to(m3)
+
 
     mapPath = "website/templates/map" + weekday + ".html" 
     print("mapPath:" +mapPath)
@@ -304,16 +415,22 @@ def build_date_map(user, req_start_date, req_end_date, req_start_time, req_end_t
             folium.PolyLine(loc, weight=5, opacity=1, color=color_).add_to(m4)
 
         
+        #calculate home and work location
     home = getHomeLoc(stops)
     workplace = getWorkLoc(stops, home)
 
     # marker home
-    popup_h = buildPopup(home["latitude"], home["longitude"])
+    popup_h = buildPopup(home, False)
     folium.Marker((home["latitude"], home["longitude"]), icon=folium.Icon(icon='home',color='blue'), popup = popup_h).add_to(m4)
 
-    # marker work
-    popup_w = buildPopup(workplace["latitude"], workplace["longitude"])
-    folium.Marker((workplace["latitude"], workplace["longitude"]), icon=folium.Icon(icon='wrench',color='red'), popup = popup_w).add_to(m4)
+
+    for entry in workplace:#print("lat: "+str(entry["latitude"])+" | long: "+str(entry["longitude"])+" id:"+str(entry["unique_id"]))
+        if len(workplace)>1:
+            popup_w = buildPopup(entry, True)
+        else:
+            popup_w = buildPopup(entry, False)
+        folium.Marker((entry["latitude"], entry["longitude"]), icon=folium.Icon(icon='wrench',color='red'), popup = popup_w).add_to(m4)
+
   
     m4.save("website/templates/map_date.html" )
    
