f23bb764727d7d8131221c37ed73d14dfecfb403
MaydayJI <105215639+MaydayJI@users.noreply.github.com>
2022-06-21 16:21:42 +0200

    Revert "Merge branch 'design_ji'"
    This reverts commit faa200138cda7c9c8a3e6f3c8974353a6a8b9c72, reversing
    changes made to e27b200224e4f9492da43238aa25692459d79c00.

--- website/models.py
+++ website/models.py
@@ -3,6 +3,8 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
@@ -11,28 +13,26 @@ class User(db.Model, UserMixin):
     home = db.relationship('Stop_h')
     work = db.relationship('Stop_w')
 
-
 class Stop_h(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    
     latitude = db.Column(db.Integer)
     longitude = db.Column(db.Integer)
     timestamp = db.Column(db.String(150))
     adress = db.Column(db.String(250))
-
 
 class Stop_w(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    
     latitude = db.Column(db.Integer)
     longitude = db.Column(db.Integer)
     timestamp = db.Column(db.String(150))
     adress = db.Column(db.String(250))
-
 
 class State(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     db_loaded = db.Column(db.Boolean)
     sigLoc_loaded = db.Column(db.Boolean)
+   
