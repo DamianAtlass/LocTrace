0bff49613c517d274ce037cc6a6ab6ce8642c6de
DamianAtlass <damianatlass@gmx.de>
2022-06-29 03:25:48 +0200

    set port

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = create_app()
 
 # makes that the flask app is only started when this file is executed
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, port=5000)
 
