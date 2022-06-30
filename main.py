6b6d52ec22cb6b9ffee5a9902deec3d22516a7e7
DamianAtlass <damianatlass@gmx.de>
2022-06-30 14:29:24 +0200

    WIP

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from website import create_app
+import cryptography
 
 app = create_app()
 
 
 # makes that the flask app is only started when this file is executed
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, port=5000)
+    app.run(debug=True, use_reloader=False, port=5000, ssl_context='adhoc')
 
